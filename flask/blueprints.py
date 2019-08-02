30852104b0154bcc3143377aea707da2abac8073
pydojo <babyfor@qq.com>
2019-08-02 16:04:29 +0800

    汉化 blueprints.py 文档字符串

--- flask/blueprints.py
+++ flask/blueprints.py
@@ -3,8 +3,8 @@
     flask.blueprints
     ~~~~~~~~~~~~~~~~
 
-    Blueprints are the recommended way to implement larger or more
-    pluggable applications in Flask 0.7 and later.
+    蓝图在 Flask 0.7 版本以后都是部署更大型
+    或更可插拔的网络应用推荐的方法。
 
     :copyright: © 2010 by the Pallets team.
     :license: BSD, see LICENSE for more details.
@@ -16,10 +16,10 @@ from .helpers import _PackageBoundObject, _endpoint_from_view_func
 
 
 class BlueprintSetupState(object):
-    """Temporary holder object for registering a blueprint with the
-    application.  An instance of this class is created by the
-    :meth:`~flask.Blueprint.make_setup_state` method and later passed
-    to all register callback functions.
+    """为使用一个网络应用注册一张蓝图而临时保留对象。
+    本类的一个实例就是通过
+    :meth:`~flask.Blueprint.make_setup_state` 方法来建立的，
+    并且稍后代入到所有注册回调函数中。
     """
 
     def __init__(self, blueprint, app, options, first_registration):
@@ -60,9 +60,8 @@ class BlueprintSetupState(object):
         self.url_defaults.update(self.options.get('url_defaults', ()))
 
     def add_url_rule(self, rule, endpoint=None, view_func=None, **options):
-        """A helper method to register a rule (and optionally a view function)
-        to the application.  The endpoint is automatically prefixed with the
-        blueprint's name.
+        """把一个 URL 规则（以及可选的一个视图函数）注册到网络应用的一个辅助方法。
+        端点是自动作为蓝图名字前缀的。
         """
         if self.url_prefix is not None:
             if rule:
@@ -81,11 +80,12 @@ class BlueprintSetupState(object):
 
 
 class Blueprint(_PackageBoundObject):
-    """Represents a blueprint.  A blueprint is an object that records
-    functions that will be called with the
-    :class:`~flask.blueprints.BlueprintSetupState` later to register functions
-    or other things on the main application.  See :ref:`blueprints` for more
-    information.
+    """表示一张蓝图。
+    一张蓝图就是一个用来记录使用
+    :class:`~flask.blueprints.BlueprintSetupState` 类
+    调用的函数的一个对象，
+    稍后把这些函数或其它东西注册到主网络应用上。
+    查看 :ref:`blueprints` 文档内容了解更多信息。
 
     .. versionadded:: 0.7
     """
@@ -132,10 +132,10 @@ class Blueprint(_PackageBoundObject):
         self.url_values_defaults = url_defaults
 
     def record(self, func):
-        """Registers a function that is called when the blueprint is
-        registered on the application.  This function is called with the
-        state as argument as returned by the :meth:`make_setup_state`
-        method.
+        """当蓝图注册到网络应用时，注册一个被调用的函数。
+        被调用的函数所含的状态与
+        :meth:`make_setup_state` 方法
+        所返回的参数是一样的。
         """
         if self._got_registered_once and self.warn_on_modifications:
             from warnings import warn
@@ -145,10 +145,10 @@ class Blueprint(_PackageBoundObject):
         self.deferred_functions.append(func)
 
     def record_once(self, func):
-        """Works like :meth:`record` but wraps the function in another
-        function that will ensure the function is only called once.  If the
-        blueprint is registered a second time on the application, the
-        function passed is not called.
+        """工作起来像 :meth:`record` 方法，
+        但把函数打包到另一个函数中，这样确保函数只被调用一次。
+        如果蓝图在网络应用上第二次注册的话，
+        代入到函数不会被调用。
         """
         def wrapper(state):
             if state.first_registration:
@@ -156,23 +156,22 @@ class Blueprint(_PackageBoundObject):
         return self.record(update_wrapper(wrapper, func))
 
     def make_setup_state(self, app, options, first_registration=False):
-        """Creates an instance of :meth:`~flask.blueprints.BlueprintSetupState`
-        object that is later passed to the register callback functions.
-        Subclasses can override this to return a subclass of the setup state.
+        """建立一个 :meth:`~flask.blueprints.BlueprintSetupState`
+        方法对象的实例，该对象稍后代入到注册的回调函数中。
+        作为子类可以覆写本方法返回一个设置状态的子类。
         """
         return BlueprintSetupState(self, app, options, first_registration)
 
     def register(self, app, options, first_registration=False):
-        """Called by :meth:`Flask.register_blueprint` to register all views
-        and callbacks registered on the blueprint with the application. Creates
-        a :class:`.BlueprintSetupState` and calls each :meth:`record` callback
-        with it.
+        """由 :meth:`Flask.register_blueprint` 方法调用注册
+        所有已经用网络应用注册在蓝图上的视图函数和回调函数。
+        建立一个 :class:`.BlueprintSetupState` 类后
+        调用每个 :meth:`record` 方法回调该类。
 
-        :param app: The application this blueprint is being registered with.
-        :param options: Keyword arguments forwarded from
-            :meth:`~Flask.register_blueprint`.
-        :param first_registration: Whether this is the first time this
-            blueprint has been registered on the application.
+        :param app: 注册本蓝图的网络应用。
+        :param options: 直接来自 :meth:`~Flask.register_blueprint` 方法
+                        的关键字参数。
+        :param first_registration: 本蓝图是否是第一次注册在网络应用上。
         """
         self._got_registered_once = True
         state = self.make_setup_state(app, options, first_registration)
@@ -187,8 +186,9 @@ class Blueprint(_PackageBoundObject):
             deferred(state)
 
     def route(self, rule, **options):
-        """Like :meth:`Flask.route` but for a blueprint.  The endpoint for the
-        :func:`url_for` function is prefixed with the name of the blueprint.
+        """像 :meth:`Flask.route` 方法一样但是给蓝图使用的。
+        对于 :func:`url_for` 函数来说，
+        端点是用蓝图的名字作前缀。
         """
         def decorator(f):
             endpoint = options.pop("endpoint", f.__name__)
@@ -197,8 +197,9 @@ class Blueprint(_PackageBoundObject):
         return decorator
 
     def add_url_rule(self, rule, endpoint=None, view_func=None, **options):
-        """Like :meth:`Flask.add_url_rule` but for a blueprint.  The endpoint for
-        the :func:`url_for` function is prefixed with the name of the blueprint.
+        """像 :meth:`Flask.add_url_rule` 方法一样但是给蓝图使用的。
+        对于 :func:`url_for` 函数来说，
+        端点是用蓝图的名字作前缀。
         """
         if endpoint:
             assert '.' not in endpoint, "Blueprint endpoints should not contain dots"
@@ -208,11 +209,11 @@ class Blueprint(_PackageBoundObject):
             s.add_url_rule(rule, endpoint, view_func, **options))
 
     def endpoint(self, endpoint):
-        """Like :meth:`Flask.endpoint` but for a blueprint.  This does not
-        prefix the endpoint with the blueprint name, this has to be done
-        explicitly by the user of this method.  If the endpoint is prefixed
-        with a `.` it will be registered to the current blueprint, otherwise
-        it's an application independent endpoint.
+        """像 :meth:`Flask.endpoint` 方法一样但是给蓝图使用的。
+        本方法中的端点不用蓝图名字作前缀，
+        要明确地由本方法的用户来实现。
+        如果端点的前缀含有一个 `.` 句号的话，
+        会注册到当前蓝图上，否则就是一个网络应用的独立端点。
         """
         def decorator(f):
             def register_endpoint(state):
@@ -222,11 +223,10 @@ class Blueprint(_PackageBoundObject):
         return decorator
 
     def app_template_filter(self, name=None):
-        """Register a custom template filter, available application wide.  Like
-        :meth:`Flask.template_filter` but for a blueprint.
+        """注册一个自定义模版过滤器，可用在网络应用范围中。
+        像 :meth:`Flask.template_filter` 方法一样但是给蓝图使用的。
 
-        :param name: the optional name of the filter, otherwise the
-                     function name will be used.
+        :param name: 可选的过滤器名字，否则使用函数的名字。
         """
         def decorator(f):
             self.add_app_template_filter(f, name=name)
@@ -234,25 +234,23 @@ class Blueprint(_PackageBoundObject):
         return decorator
 
     def add_app_template_filter(self, f, name=None):
-        """Register a custom template filter, available application wide.  Like
-        :meth:`Flask.add_template_filter` but for a blueprint.  Works exactly
-        like the :meth:`app_template_filter` decorator.
+        """注册一个自定义模版过滤器，可用在网络应用范围中。
+        像 :meth:`Flask.add_template_filter` 方法一样但是给蓝图使用的。
+        工作起来完全像 :meth:`app_template_filter` 方法装饰器一样。
 
-        :param name: the optional name of the filter, otherwise the
-                     function name will be used.
+        :param name: 可选的过滤器名字，否则使用函数的名字。
         """
         def register_template(state):
             state.app.jinja_env.filters[name or f.__name__] = f
         self.record_once(register_template)
 
     def app_template_test(self, name=None):
-        """Register a custom template test, available application wide.  Like
-        :meth:`Flask.template_test` but for a blueprint.
+        """注册一个自定义模版测试，可用在网络应用范围中。
+        像 :meth:`Flask.template_test` 方法一样但是给蓝图使用的。
 
         .. versionadded:: 0.10
 
-        :param name: the optional name of the test, otherwise the
-                     function name will be used.
+        :param name: 可选的测试名字，否则使用函数的名字。
         """
         def decorator(f):
             self.add_app_template_test(f, name=name)
@@ -260,27 +258,25 @@ class Blueprint(_PackageBoundObject):
         return decorator
 
     def add_app_template_test(self, f, name=None):
-        """Register a custom template test, available application wide.  Like
-        :meth:`Flask.add_template_test` but for a blueprint.  Works exactly
-        like the :meth:`app_template_test` decorator.
+        """注册一个自定义模版测试，可用在网络应用范围中。
+        像 :meth:`Flask.add_template_test` 方法一样但是给蓝图使用的。
+        工作起来完全像 :meth:`app_template_test` 方法装饰器一样。
 
         .. versionadded:: 0.10
 
-        :param name: the optional name of the test, otherwise the
-                     function name will be used.
+        :param name: 可选的测试名字，否则使用函数的名字。
         """
         def register_template(state):
             state.app.jinja_env.tests[name or f.__name__] = f
         self.record_once(register_template)
 
     def app_template_global(self, name=None):
-        """Register a custom template global, available application wide.  Like
-        :meth:`Flask.template_global` but for a blueprint.
+        """注册一个自定义模版全局对象，可用在网络应用范围中。
+        像 :meth:`Flask.template_global` 方法一样但是给蓝图使用的。
 
         .. versionadded:: 0.10
 
-        :param name: the optional name of the global, otherwise the
-                     function name will be used.
+        :param name: 可选的全局对象名，否则使用函数的名字。
         """
         def decorator(f):
             self.add_app_template_global(f, name=name)
@@ -288,99 +284,99 @@ class Blueprint(_PackageBoundObject):
         return decorator
 
     def add_app_template_global(self, f, name=None):
-        """Register a custom template global, available application wide.  Like
-        :meth:`Flask.add_template_global` but for a blueprint.  Works exactly
-        like the :meth:`app_template_global` decorator.
+        """注册一个自定义模版全局对象，可用在网络应用范围中。
+        像 :meth:`Flask.add_template_global` 方法一样但是给蓝图使用的。
+        工作起来完全像 :meth:`app_template_global` 方法装饰器一样。
 
         .. versionadded:: 0.10
 
-        :param name: the optional name of the global, otherwise the
-                     function name will be used.
+        :param name: 可选的全局对象名，否则使用函数的名字。
         """
         def register_template(state):
             state.app.jinja_env.globals[name or f.__name__] = f
         self.record_once(register_template)
 
     def before_request(self, f):
-        """Like :meth:`Flask.before_request` but for a blueprint.  This function
-        is only executed before each request that is handled by a function of
-        that blueprint.
+        """像 :meth:`Flask.before_request` 方法一样但是给蓝图使用的。
+        这个函数只在由蓝图的一个函数处理的每个请求之前被执行。
         """
         self.record_once(lambda s: s.app.before_request_funcs
             .setdefault(self.name, []).append(f))
         return f
 
     def before_app_request(self, f):
-        """Like :meth:`Flask.before_request`.  Such a function is executed
-        before each request, even if outside of a blueprint.
+        """像 :meth:`Flask.before_request` 方法一样。
+        这样一个函数是在每个请求之前被执行，
+        即使请求是蓝图以外的，这个函数也会被执行。
         """
         self.record_once(lambda s: s.app.before_request_funcs
             .setdefault(None, []).append(f))
         return f
 
     def before_app_first_request(self, f):
-        """Like :meth:`Flask.before_first_request`.  Such a function is
-        executed before the first request to the application.
+        """像 :meth:`Flask.before_first_request` 方法一样。
+        这样的一个函数是在到达网络应用的第一个请求之前被执行。
         """
         self.record_once(lambda s: s.app.before_first_request_funcs.append(f))
         return f
 
     def after_request(self, f):
-        """Like :meth:`Flask.after_request` but for a blueprint.  This function
-        is only executed after each request that is handled by a function of
-        that blueprint.
+        """像 :meth:`Flask.after_request` 方法一样但是给蓝图使用的。
+        这个函数只在由蓝图的一个函数处理的每个请求之后被执行。
         """
         self.record_once(lambda s: s.app.after_request_funcs
             .setdefault(self.name, []).append(f))
         return f
 
     def after_app_request(self, f):
-        """Like :meth:`Flask.after_request` but for a blueprint.  Such a function
-        is executed after each request, even if outside of the blueprint.
+        """像 :meth:`Flask.after_request` 方法一样但是给蓝图使用的。
+        这样一个函数只在每个请求之后被执行，
+        即使请求是蓝图以外的，这个函数也会被执行。
         """
         self.record_once(lambda s: s.app.after_request_funcs
             .setdefault(None, []).append(f))
         return f
 
     def teardown_request(self, f):
-        """Like :meth:`Flask.teardown_request` but for a blueprint.  This
-        function is only executed when tearing down requests handled by a
-        function of that blueprint.  Teardown request functions are executed
-        when the request context is popped, even when no actual request was
-        performed.
+        """像 :meth:`Flask.teardown_request` 方法一样但是给蓝图使用的。
+        这个函数只在由蓝图的一个函数处理的释放请求时被执行。
+        释放请求函数都在请求语境被删除时被执行，
+        甚至当没有实际请求执行时，释放请求函数也都被执行。
         """
         self.record_once(lambda s: s.app.teardown_request_funcs
             .setdefault(self.name, []).append(f))
         return f
 
     def teardown_app_request(self, f):
-        """Like :meth:`Flask.teardown_request` but for a blueprint.  Such a
-        function is executed when tearing down each request, even if outside of
-        the blueprint.
+        """像 :meth:`Flask.teardown_request` 方法一样但是给蓝图使用的。
+        这样一个函数是在释放每个请求时被执行，
+        即使请求是蓝图以外的，这个函数也会被执行。
         """
         self.record_once(lambda s: s.app.teardown_request_funcs
             .setdefault(None, []).append(f))
         return f
 
     def context_processor(self, f):
-        """Like :meth:`Flask.context_processor` but for a blueprint.  This
-        function is only executed for requests handled by a blueprint.
+        """像 :meth:`Flask.context_processor` 方法一样但是给蓝图使用的。
+        这个函数只为蓝图处理的请求而执行。
         """
         self.record_once(lambda s: s.app.template_context_processors
             .setdefault(self.name, []).append(f))
         return f
 
     def app_context_processor(self, f):
-        """Like :meth:`Flask.context_processor` but for a blueprint.  Such a
-        function is executed each request, even if outside of the blueprint.
+        """像 :meth:`Flask.context_processor` 方法一样但是给蓝图使用的。
+        这样一个函数为每个请求而执行，
+        即使请求是蓝图以外的，这个函数也会被执行。
         """
         self.record_once(lambda s: s.app.template_context_processors
             .setdefault(None, []).append(f))
         return f
 
     def app_errorhandler(self, code):
-        """Like :meth:`Flask.errorhandler` but for a blueprint.  This
-        handler is used for all requests, even if outside of the blueprint.
+        """像 :meth:`Flask.errorhandler` 方法一样但是给蓝图使用的。
+        这个处理器是为所有请求而使用的，
+        即使请求是蓝图以外的，也可以使用这个处理器。
         """
         def decorator(f):
             self.record_once(lambda s: s.app.errorhandler(code)(f))
@@ -388,47 +384,45 @@ class Blueprint(_PackageBoundObject):
         return decorator
 
     def url_value_preprocessor(self, f):
-        """Registers a function as URL value preprocessor for this
-        blueprint.  It's called before the view functions are called and
-        can modify the url values provided.
+        """为本蓝图把一个函数注册成 URL 值处理器。
+        在视图函数被调用之前调用处理器，
+        并且可以修改提供的 URL 值。
         """
         self.record_once(lambda s: s.app.url_value_preprocessors
             .setdefault(self.name, []).append(f))
         return f
 
     def url_defaults(self, f):
-        """Callback function for URL defaults for this blueprint.  It's called
-        with the endpoint and values and should update the values passed
-        in place.
+        """为本蓝图针对 URL 默认值的回调函数。
+        回调函数被调用时含有端点和值，
+        并且应该更新代入的值。
         """
         self.record_once(lambda s: s.app.url_default_functions
             .setdefault(self.name, []).append(f))
         return f
 
     def app_url_value_preprocessor(self, f):
-        """Same as :meth:`url_value_preprocessor` but application wide.
-        """
+        """与 :meth:`url_value_preprocessor` 方法一样但是在蓝图范围中."""
         self.record_once(lambda s: s.app.url_value_preprocessors
             .setdefault(None, []).append(f))
         return f
 
     def app_url_defaults(self, f):
-        """Same as :meth:`url_defaults` but application wide.
-        """
+        """与 :meth:`url_defaults` 方法一样但是在蓝图范围中。."""
         self.record_once(lambda s: s.app.url_default_functions
             .setdefault(None, []).append(f))
         return f
 
     def errorhandler(self, code_or_exception):
-        """Registers an error handler that becomes active for this blueprint
-        only.  Please be aware that routing does not happen local to a
-        blueprint so an error handler for 404 usually is not handled by
-        a blueprint unless it is caused inside a view function.  Another
-        special case is the 500 internal server error which is always looked
-        up from the application.
+        """只为本蓝图注册一个激活的错误处理器。
+        要意识到路由不出现在本地到一个蓝图上，
+        所以一个蓝图错误处理器常常不对 404 进行处理，
+        除非 404 发生在一个视图函数内部。
+        另一个具体情况就是 500 内部服务器错误，
+        该错误总会从网络应用中来查找。
 
-        Otherwise works as the :meth:`~flask.Flask.errorhandler` decorator
-        of the :class:`~flask.Flask` object.
+        否则工作起来就与 :class:`~flask.Flask` 类对象的
+        :meth:`~flask.Flask.errorhandler` 方法装饰器一样了。
         """
         def decorator(f):
             self.record_once(lambda s: s.app._register_error_handler(
@@ -437,10 +431,11 @@ class Blueprint(_PackageBoundObject):
         return decorator
 
     def register_error_handler(self, code_or_exception, f):
-        """Non-decorator version of the :meth:`errorhandler` error attach
-        function, akin to the :meth:`~flask.Flask.register_error_handler`
-        application-wide function of the :class:`~flask.Flask` object but
-        for error handlers limited to this blueprint.
+        """是 :meth:`errorhandler` 方法的非装饰器版本，
+        类似 :class:`~flask.Flask` 类对象网络应用范围中
+        函数的
+        :meth:`~flask.Flask.register_error_handler` 方法，
+        但是仅限提供给蓝图的错误处理器。
 
         .. versionadded:: 0.11
         """
